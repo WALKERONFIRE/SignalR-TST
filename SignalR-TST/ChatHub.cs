3797f015da3e0d5bfb1f9d5d7c62e1241fdecd5e
WALKERONFIRE <ahmed.hhh35@gmail.com>
2024-07-13 06:41:32 +0300

    Add project files.

---
+++ SignalR-TST/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_TST
+{
+    public class ChatHub : Hub
+    {
+    }
+}
---
+++ SignalR-TST/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace SignalR_TST.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string name { get; set; }
+
+    }
+}
